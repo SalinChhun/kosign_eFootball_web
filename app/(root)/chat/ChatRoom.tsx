d2f547dd5c6f67d4f87fccd087528e94f649dfae
SalinChhun <salinchhun123079@gmail.com>
2025-02-18 16:09:24 +0700

    comment chat page

--- app/(root)/chat/ChatRoom.tsx
+++ app/(root)/chat/ChatRoom.tsx
@@ -1,3 +1,4 @@
+/*
 "use client"
 import React, {useEffect, useState} from 'react'
 import {over} from 'stompjs';
@@ -209,4 +210,4 @@ const ChatRoom = () => {
     )
 }
 
-export default ChatRoom
+export default ChatRoom*/
--- app/(root)/chat/page.tsx
+++ app/(root)/chat/page.tsx
@@ -1,29 +1,15 @@
-'use client';
+/*
+"use client";
+import ChatRoom from "@/app/(root)/chat/ChatRoom";
+import {Suspense} from "react";
 
-import React, { useEffect, useState } from 'react';
-import { useSearchParams } from "next/navigation";
-
-const ChatRoom = () => {
-    const searchParams = useSearchParams();
-    const [userName, setUserName] = useState("");
-
-    useEffect(() => {
-        setUserName(searchParams.get("userName") || ""); // Ensure reactivity
-    }, [searchParams]);
-
-    const [userData, setUserData] = useState({
-        username: userName, // Use state instead
-        receiverName: '',
-        connected: false,
-        message: ''
-    });
+export default function ChatPage() {
 
     return (
-        <div>
-            <p>Welcome, {userData.username}!</p>
-            {/* Rest of your component */}
-        </div>
-    );
-};
+        <Suspense fallback={<div>Loading...</div>}>
+            <ChatRoom />
+        </Suspense>
 
-export default ChatRoom;
+    );
+}
+*/
