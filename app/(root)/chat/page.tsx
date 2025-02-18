27cd81b417da968c3709fb3695c5336fd3d23e99
SalinChhun <salinchhun123079@gmail.com>
2025-02-18 15:04:58 +0700

    fix chat page build

--- app/(root)/chat/page.tsx
+++ app/(root)/chat/page.tsx
@@ -1,12 +1,13 @@
 "use client"
 import ChatRoom from "@/app/(root)/chat/ChatRoom";
+import {Suspense} from "react";
 
 export default function ChatPage() {
 
     return (
-        <>
+        <Suspense fallback={<div>Loading...</div>}>
             <ChatRoom/>
-        </>
+        </Suspense>
 
     );
 }
