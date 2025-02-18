76e13d121c409ea5e4e5ce1a75e66e020905eac5
SalinChhun <salinchhun123079@gmail.com>
2025-02-18 14:09:32 +0700

    fix home page

--- app/(home)/layout.tsx
+++ app/(home)/layout.tsx
@@ -2,15 +2,23 @@ import {PropsWithChildren} from "react";
 import NavBar from "@/app/components/shared/NavBar";
 import "@/app/styles/globals.css";
 import "@/app/styles/we-football.css";
+import "@/app/styles/home-page.css";
+import {ShootingStars} from "@/app/components/shared/ShootingStars";
+import {StarsBackground} from "@/app/components/shared/StarsBackground";
 
-export default function HomeLayout({ children }: PropsWithChildren) {
+export default function HomeLayout({children}: PropsWithChildren) {
 
     return (
         <html lang="en">
-            <body className="body" style={{overflowX: "hidden"}}>
-                <NavBar/>
-                {children}
-            </body>
+        <body className="body" style={{overflowX: "hidden"}}>
+        <NavBar/>
+        {/*<div className="h-full bg-neutral-900 relative w-full">*/}
+        <div className="h-full bg-neutral-900 relative w-full">
+            {children}
+            <ShootingStars/>
+            <StarsBackground/>
+        </div>
+        </body>
         </html>
     );
 
--- app/components/shared/NavBar.tsx
+++ app/components/shared/NavBar.tsx
@@ -28,7 +28,7 @@ const NavBar = () => {
             {/* Full Menu for Desktop */}
             <ul className="navbar-menu">
                 <li className="navbar-item">
-                    <Link href="/scores" className={pathname.startsWith('/scores') ? 'cur_act' : ''}>
+                    <Link href="/" className={pathname === '/' ? 'cur_act' : ''}>
                         <svg className="ks_img svg-icon" xmlns="http://www.w3.org/2000/svg" fill="#FFFFFFFF"
                              viewBox="0 0 20 20">
                             <path
@@ -61,7 +61,8 @@ const NavBar = () => {
                 </li>
                 <li className="navbar-item">
                     <Link href="/teams" className={pathname.startsWith('/teams') ? 'cur_act' : ''}>
-                        <svg className="icon flat-color ks_img svg-icon" width="800px" height="800px" viewBox="0 0 36 36" xmlns="http://www.w3.org/2000/svg"
+                        <svg className="icon flat-color ks_img svg-icon" width="800px" height="800px"
+                             viewBox="0 0 36 36" xmlns="http://www.w3.org/2000/svg"
                              aria-hidden="true" role="img"
                              preserveAspectRatio="xMidYMid meet" fill="#ff6b00">
                             <g id="SVGRepo_bgCarrier" strokeWidth="0"/>
@@ -119,7 +120,14 @@ const NavBar = () => {
             {/* Navbar Actions (Sign in and Search) */}
             <div className="navbar-actions">
                 <Link href="/signin" className="sign-in-button">Sign in</Link>
-                <span className="search-icon">🔍</span>
+                <span className="search-icon">
+                    <svg widths={25} height={25} viewBox="0 0 24 24" fill="#ffffff"
+                         xmlns="http://www.w3.org/2000/svg">
+                        <path fill-rule="evenodd" clip-rule="evenodd"
+                          d="M4 11C4 7.13401 7.13401 4 11 4C14.866 4 18 7.13401 18 11C18 14.866 14.866 18 11 18C7.13401 18 4 14.866 4 11ZM11 2C6.02944 2 2 6.02944 2 11C2 15.9706 6.02944 20 11 20C13.125 20 15.078 19.2635 16.6177 18.0319L20.2929 21.7071C20.6834 22.0976 21.3166 22.0976 21.7071 21.7071C22.0976 21.3166 22.0976 20.6834 21.7071 20.2929L18.0319 16.6177C19.2635 15.078 20 13.125 20 11C20 6.02944 15.9706 2 11 2Z"
+                          fill="#ffffff"/>
+                    </svg>
+                </span>
                 <span className="menu-icon" onClick={openMenu}>☰</span>
             </div>
 
